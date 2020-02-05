2990224b189edc15b54e83e21db0cac9c8b42494
Naimur Rahman Sourov <naimur.rahman103@gmail.com>
2020-02-05 15:38:46 +0600

    Cleanup

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const getTimeZone = require("./timezone");
 // REF: https://www.codeproject.com/Questions/1010213/How-do-I-sort-timezone-by-offset-value
 const sortByGmt = () => {
   const timeZone = getTimeZone();
-  return timeZone.sort(function(a, b) {
-    var re = /^\(GMT([+-]\d{1,2}):(\d{1,2})\).*$/;
-    var aOffset = parseFloat(a.replace(re, "$1.$2"));
-    var bOffset = parseFloat(b.replace(re, "$1.$2"));
+  return timeZone.sort((a, b) => {
+    const re = /^\(GMT([+-]\d{1,2}):(\d{1,2})\).*$/;
+    const aOffset = parseFloat(a.replace(re, "$1.$2"));
+    const bOffset = parseFloat(b.replace(re, "$1.$2"));
     return aOffset < bOffset ? -1 : aOffset > bOffset ? 1 : 0;
   });
 };
-console.log(JSON.stringify(sortByGmt()))
 module.exports = sortByGmt;
